5df32c78b1edbbf4e926a7f6d78670a6ab667b75
Nelson Ding <nding17@outlook.com>
2019-11-10 22:18:22 -0500

    update address

--- scripts/rent.com_scraping.py
+++ scripts/rent.com_scraping.py
@@ -179,7 +179,7 @@ class rent_dot_com:
 	        addr = self._get_address(address_tag, hdr)
 	        
 	        room_tags = soup.find_all('div', '_1ECa-')
-	        
+
 	        for rt in room_tags:
 	            room_table = rt.find('table', '_1GkPp F4skJ')
 	            room_tbody = room_table.find('tbody')
@@ -254,7 +254,7 @@ if __name__ == '__main__':
 		df = pd.DataFrame([], columns=cols)
 		df.to_csv('./rent_dot_com.csv')
 
-	for i, batch_urls in enumerate(urls_chunk[33:]):
+	for i, batch_urls in enumerate(urls_chunk[40:]):
 		print(batch_urls)
 		rdc.scrape_apt_data(batch_urls, verbose=True)
 		data = rdc.apt_data
