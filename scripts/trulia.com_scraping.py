d270c01a822c33dba3428a862a67dac923ce473a
Nelson Ding <nding17@outlook.com>
2019-11-19 10:15:29 -0500

    saving images

--- scripts/trulia.com_scraping.py
+++ scripts/trulia.com_scraping.py
@@ -23,10 +23,11 @@ class trulia_dot_com:
         self._apt_urls_buy = []
         self._apt_urls_rent = []
         self._apt_urls_sold = []
+        self._overhead = 'https://www.trulia.com'
 
     def _get_buy_webpage(self, pg_num, htype):
     
-        overhead = 'https://www.trulia.com'
+        overhead = self._overhead
         dangle = 'for_sale'
 
         city = self._city\
@@ -55,6 +56,7 @@ class trulia_dot_com:
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) \
                 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
         response = requests.get(url, headers=headers)
+        results = response.content
         if not response.status_code == 404:
             soup = BeautifulSoup(results, 'lxml')
         return soup
@@ -83,14 +85,15 @@ class trulia_dot_com:
             apt_tags = soup.find_all('div', class_=apt_class)
             
             apt_link_tags = [tag.find('a') for tag in apt_tags]
-            apt_urls = [tag['href'] for tag in apt_link_tags]
+            apt_urls = [f"{self._overhead}/{tag['href']}" for tag in apt_link_tags]
         
         return apt_urls
 
     def _get_buy_apt_urls_ensemble(self,
                                    htype=['house', 
                                           'multi-family'],
-                                   verbose=False,):
+                                   verbose=False,
+                                   test=False):
         stop = False
         urls_ensemble = ['']
         pg_num = 1
@@ -104,6 +107,9 @@ class trulia_dot_com:
                 stop = True
             urls_ensemble.append(urls_per_pg)
             pg_num += 1
+
+            if test and pg_num == 5:
+                break
         
         def _flatten(lst):
             for el in lst:
@@ -117,11 +123,111 @@ class trulia_dot_com:
             print('last 2 pages removed since they are the same.')
         return urls_ensemble_cut
 
+    def _load_json(self, soup):
+        jfile = soup.find('script', attrs={
+            'id': '__NEXT_DATA__',
+            'type': 'application/json',
+        }).get_text()
+        
+        jdict = json.loads(jfile)
+        return jdict
+
+    def _get_img_urls(self, jdict):
+        pics = jdict['props']['homeDetails']['media']['photos']
+        urls = [pic['url']['mediumSrc'] for pic in pics]
+        return urls
+
+    def _save_images(self, 
+                     img_urls, 
+                     data_path, 
+                     img_type, 
+                     address):
+        try:
+            current_path = os.getcwd()
+            os.chdir(data_path)
+            
+            if not os.path.exists(img_type):
+                os.mkdir(img_type)
+            os.chdir(img_type)
+            
+            if not os.path.exists(address):
+                os.mkdir(address)
+            os.chdir(address)
+            
+            for i, img_url in enumerate(img_urls):
+                img_data = requests.get(img_url).content
+                with open(f'img{i}.jpg', 'wb') as handler:
+                    handler.write(img_data)
+                    
+            os.chdir(current_path)
+            return 1
+        
+        except:
+            return 0
+
+    def _get_address(self, jdict):
+        try:
+            loc_dict = jdict['props']['homeDetails']['location']
+            state = loc_dict['stateCode']
+            city = loc_dict['city']
+            zipcode = loc_dict['zipCode']
+            street = loc_dict['formattedLocation']
+            try:
+                neighborhood = loc_dict['neighborhoodName']
+            except:
+                neighborhood = None
+            return street, city, state, zipcode, neighborhood
+        except:
+            return None, None, None, None, None
+
+    def _get_price(self, jdict):
+        try:
+            price_dict = jdict['props']['homeDetails']['price']
+            return float(price_dict['price'])
+        except:
+            return None
+
+    def _get_bedrooms_bathrooms(self, jdict):
+        try:
+            pattern = r'[-+]?\d*\.\d+|\d+'
+            bed_dict = jdict['props']['homeDetails']['bedrooms']
+            bath_dict = jdict['props']['homeDetails']['bathrooms']
+            bedrooms = re.findall(pattern, bed_dict['summaryBedrooms'])[0]
+            bathrooms = re.findall(pattern, bath_dict['summaryBathrooms'])[0]
+            return float(bedrooms), float(bathrooms)
+        except:
+            return np.nan, np.nan
+
+    def _get_apt_features(self, jdict):
+        try:
+            fdict_list = jdict['props']['homeDetails']['features']['attributes']
+            features = []
+            for fdict in fdict_list:
+                try:
+                    value = fdict['formattedValue']
+                    try:
+                        key = fdict['formattedName']
+                        features.append(f'{key}:{value}')
+                    except:
+                        features.append(value)
+                except:
+                    next
+            return ' | '.join(features)
+        except:
+            return None
+
 
 if __name__ == '__main__':
 
     tdc = trulia_dot_com('philadelphia', 'pa')
-    urls_all = tdc._get_buy_apt_urls_ensemble(verbose=True)
-    print(len(urls_all))
+    urls_all = tdc._get_buy_apt_urls_ensemble(verbose=True, test=True)
+
+    for url in urls_all:
+        soup = tdc._get_soup(url)
+        jdict = tdc._load_json(soup)
+        img_urls = tdc._get_img_urls(jdict)
+        address = tdc._get_address(jdict)[0].upper()
+        data_path = '../data/sample/trulia/imgdata'
+        tdc._save_images(img_urls, data_path, 'buy', address)
 
 
