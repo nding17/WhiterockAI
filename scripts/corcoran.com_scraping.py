960081babe31666c11970b5eef6d02b22ff4c3f7
Nelson Ding <nding17@outlook.com>
2019-12-03 22:19:26 -0500

    scrolling tested

--- scripts/corcoran.com_scraping.py
+++ scripts/corcoran.com_scraping.py
@@ -12,6 +12,50 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
+class CONST:
+    MAIN_QUERY = 'https://www.corcoran.com/nyc-real-estate/for-sale/search?'\
+                  'neighborhoods=battery-park-city%2Cbeekman%2Ccentral-park-south'\
+                  '%2Cchelsea-hudson-yards%2Cchinatown%2Cclinton%2Ceast-harlem'\
+                  '%2Ceast-village%2Cfinancial-district%2Cflatiron%2Cgramercy'\
+                  '%2Cgreenwich-village%2Chamilton-heights%2Charlem%2Cinwood'\
+                  '%2Clower-east-side%2Cmidtown-east%2Cmidtown-west'\
+                  '%2Cmorningside-heights%2Cmurray-hill%2Croosevelt-island'\
+                  '%2Csoho-nolita%2Csutton-area%2Ctribeca%2Cupper-east-side'\
+                  '%2Cupper-west-side%2Cwashington-heights%2Cwest-village'\
+                  '%2Ccarnegie-hill%2Ckips-bay%2Cno-mad%2Cbath-beach%2Cbensonhurst'\
+                  '%2Cbay-ridge%2Cbedford-stuyvesant%2Cbergen-beach%2Cboerum-hill'\
+                  '%2Cborough-park%2Cbrighton-beach%2Cbrooklyn-heights%2Cbrownsville'\
+                  '%2Cbushwick%2Ccanarsie%2Ccarroll-gardens%2Cclinton-hill%2Ccobble-hill'\
+                  '%2Cconey-island%2Ccrown-heights%2Ccypress-hill%2Cditmas-park'\
+                  '%2Cdowntown-brooklyn%2Cdyker-heights%2Ceast-flatbush%2Ceast-new-york'\
+                  '%2Cflatbush%2Cflatlands%2Cfort-greene%2Cgowanus%2Cgravesend'\
+                  '%2Cgreenpoint%2Cgreenwood%2Ckensington%2Clefferts-gardens'\
+                  '%2Cmanhattan-beach%2Cmapleton%2Cmarine-park%2Cmidwood'\
+                  '%2Cmill-basin%2Cnew-lots%2Cpark-slope%2Cprospect-heights'\
+                  '%2Cprospect-park-south%2Cred-hook%2Csea-gate%2Csheepshead-bay'\
+                  '%2Cspring-creek%2Cstarrett-city%2Csunset-park%2Cdumbo-vinegar-hill'\
+                  '%2Cweeksville%2Cwilliamsburg%2Cwindsor-terrace%2Cocean-parkway'\
+                  '%2Cgerritsen-beach%2Cbrooklyn-navy-yard%2Ccolumbia-waterfront'\
+                  '%2Castoria%2Cbelle-harbor%2Cforest-hills%2Cjackson-heights%2Ckew-gardens'\
+                  '%2Clong-island-city%2Csunnyside%2Cwoodside%2Cridgewood%2Cflushing'\
+                  '%2Cfresh-meadows%2Cjamaica%2Csouth-jamaica%2Cmaspeth%2Cglendale'\
+                  '%2Cmiddle-village%2Cwoodhaven%2Celmhurst%2Ceast-elmhurst%2Ccorona'\
+                  '%2Ccollege-point%2Cwhitestone%2Cqueens-village%2Cbellerose%2Chollis'\
+                  '%2Cst-albans%2Ccambria-heights%2Cozone-park%2Csouth-ozone-park'\
+                  '%2Choward-beach%2Crichmond-hills%2Cspringfield-gardens%2Claurelton'\
+                  '%2Crockaway-beach%2Cbriarwood%2Cbroad-channel%2Cfloral-park%2Cglen-oaks'\
+                  '%2Cjamaica-hills%2Ckew-gardens-hills%2Clittle-neck%2Cnew-hyde-park'\
+                  '%2Crego-park%2Crochdale%2Crockaway%2Crosedale%2Criverdale%2Callerton'\
+                  '%2Cbaychester%2Cbedford-park%2Cbelmont%2Ccastle-hill%2Ccity-island'\
+                  '%2Cco-op-city%2Ccountry-club%2Ceast-tremont%2Ceastchester%2Cedenwald'\
+                  '%2Cedgewater-park%2Cfordham%2Chighbridge%2Chunts-point%2Ckingsbridge'\
+                  '%2Claconia%2Clongwood%2Cmelrose%2Cmorris-heights%2Cmorris-park%2Cmorrisania'\
+                  '%2Cmott-haven%2Cnorwood%2Cparkchester%2Cpelham-bay%2Cpelham-gardens'\
+                  '%2Cpelham-parkway%2Cschuylerville%2Csoundview%2Cthroggs-neck%2Ctremont'\
+                  '%2Cuniversity-heights%2Cvan-nest%2Cwakefield%2Cwilliamsbridge'\
+                  '%2Cwoodlawn&keywordSearch=houses%2Ctownhouses'
+
 class corcoran_dot_com:
 
     ############################
@@ -85,38 +129,24 @@ class corcoran_dot_com:
                     return soup
             raise ValueError(f'FAILED to get soup for apt url {url}')
 
-    def _get_apt_info(self, apt_path, wait):
-    try:
-        url_path = "a[@class='ListingCard__TopSectionLink-k9s72e-17 icXLMN']"
-        full_path = f'{apt_path}/{url_path}'
-        element = wait.until(EC.presence_of_element_located((By.XPATH, full_path)))
-        href = element.get_attribute('href')
-        return href 
-    except:
-        print('can not find apartment')
-        return None
+    def _get_apt_url(self, apt_path, wait):
+        try:
+            url_path = "a[@class='ListingCard__TopSectionLink-k9s72e-17 icXLMN']"
+            full_path = f'{apt_path}/{url_path}'
+            element = wait.until(EC.presence_of_element_located((By.XPATH, full_path)))
+            href = element.get_attribute('href')
+            return href 
+        except:
+            print('can not find apartment')
+            return None
 
-    def _get_apt_info_batches(self, start, end, wait):
+    def _get_apt_url_batches(self, start, end, wait):
         results_batch = []
         for i in range(start, end):
             sibling_path = f"//div[@class='ListingCard__ListingCardWrapper-k9s72e-7 bxPua']/following-sibling::div[{i}]"
-            href = self._get_apt_info(sibling_path, wait)
+            href = self._get_apt_url(sibling_path, wait)
             results_batch.append(href)
-        return results_batch
-
-    def _scroll_down(self, scroll_pg, wait):
-        try:
-            dest = f"//div[@class='ListingCard__ListingCardWrapper-k9s72e-7 bxPua']/following-sibling::div[{scroll_pg}]"
-            browser.implicitly_wait(5)
-            elem_dest = wait.until(EC.presence_of_element_located((By.XPATH, dest)))
-            browser.execute_script('arguments[0].scrollIntoView(true)', elem_dest)
-            
-            buffer_dest = f"//div[@class='ListingCard__ListingCardWrapper-k9s72e-7 bxPua']/following-sibling::div[{scroll_pg+5}]"
-            browser.implicitly_wait(5)
-            elem_buffer = wait.until(EC.presence_of_element_located((By.XPATH, buffer_dest)))
-            browser.execute_script('arguments[0].scrollIntoView(true)', elem_buffer)
-        except:
-            print(f'scrolling failed')
+        return results_batch 
             
     def _get_total_apt_num(self, url):
         soup = self._soup_attempts(url)
@@ -140,25 +170,30 @@ class corcoran_dot_com:
             scroll_buffer = scroll_pg+30
         
         return scroll_buffer
+
+    def _get_browser(self, chromedriver):
+        browser = webdriver.Chrome(executable_path=chromedriver)
+        browser.get(CONST.MAIN_QUERY)
+        wait = WebDriverWait(browser, 20)
+        return browser, wait
             
-    def _scroll_down(self, scroll_pg, wait):
+    def _scroll_down(self, scroll_pg, browser, wait):
         try:
             dest = f"//div[@class='ListingCard__ListingCardWrapper-k9s72e-7 bxPua']/following-sibling::div[{scroll_pg}]"
             browser.implicitly_wait(5)
             elem_dest = wait.until(EC.presence_of_element_located((By.XPATH, dest)))
             browser.execute_script('arguments[0].scrollIntoView(true)', elem_dest)
-            time.sleep(5)
-            scroll_buffer = self._buffer_page(scroll_pg)
             
-            buffer_dest = f"//div[@class='ListingCard__ListingCardWrapper-k9s72e-7 bxPua']/following-sibling::div[{scroll_buffer}]"
+            buffer_dest = f"//div[@class='ListingCard__ListingCardWrapper-k9s72e-7 bxPua']/following-sibling::div[{scroll_pg+5}]"
             browser.implicitly_wait(5)
             elem_buffer = wait.until(EC.presence_of_element_located((By.XPATH, buffer_dest)))
             browser.execute_script('arguments[0].scrollIntoView(true)', elem_buffer)
         except:
-            print('scrolling failed') 
+            print(f'scrolling failed')
 
     def _keep_scrolling_down(self, 
                              total_apt_num, 
+                             browser, 
                              wait, 
                              verbose=False, 
                              test=False):
@@ -170,41 +205,47 @@ class corcoran_dot_com:
         
         for i in range(nbatches):
             scroll_pg = 49*(i+1)
-            self._scroll_down(scroll_pg, wait)
+            self._scroll_down(scroll_pg, browser, wait)
             if verbose:
                 print(f'page {i+1} scrolled')
             
             start, end = 49*i, 49*(i+1)
             if i == 0:
                 start = 1
-            results += self._get_apt_info_batches(start, end, wait)
+            results += self._get_apt_url_batches(start, end, wait)
             
             if verbose:
                 print(f'results for page {i+1} obtained')
                 
         return results
 
-    def _get_apt_urls(self, 
-                      url, 
+    def _get_apt_urls(self,
+                      browser, 
                       wait, 
                       verbose=False, 
                       test=False):
         first_apt_path = f"//div[@class='ListingCard__ListingCardWrapper-k9s72e-7 bxPua']"
-        first_apt_url = self._get_apt_info(first_apt_path, wait)
+        first_apt_url = self._get_apt_url(first_apt_path, wait)
         results = [first_apt_url]
-        total_apt_num = self._get_total_apt_num(url)    
-        results += self._keep_scrolling_down(total_apt_num, wait, verbose, test)
+        total_apt_num = self._get_total_apt_num(CONST.MAIN_QUERY) 
+
+        results += self._keep_scrolling_down(total_apt_num, 
+                                             browser, 
+                                             wait, 
+                                             verbose, 
+                                             test)
+
         results_final = list(filter(lambda x: x!=None, results))
         return results_final
 
     def _get_apt_address(self, soup_apt):
-    try:
-        address = soup_apt.find('div', attrs={'data-name': 'col-md-8'})\
-                          .find('h1')\
-                          .get_text()
-        return address
-    except:
-        return None
+        try:
+            address = soup_apt.find('div', attrs={'data-name': 'col-md-8'})\
+                              .find('h1')\
+                              .get_text()
+            return address
+        except:
+            return None
 
     def _get_apt_listing_type(self, soup_apt):
         try:
@@ -271,5 +312,14 @@ class corcoran_dot_com:
         except:
             return None
 
+if __name__ == '__main__':
     
+    chromedriver = '/Users/itachi/Downloads/Chrome/chromedriver'
+
+    cdc = corcoran_dot_com()
+    browser, wait = cdc._get_browser(chromedriver)
+    cdc._get_apt_urls(browser, 
+                      wait, 
+                      verbose=True, 
+                      test=True)
 
