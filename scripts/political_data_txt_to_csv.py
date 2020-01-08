ad55b8b893c2b61a28bc4b6a0f7705f45d12e6a1
Nelson Ding <nding17@outlook.com>
2020-01-08 16:46:22 -0500

    process data

--- scripts/political_data_txt_to_csv.py
+++ scripts/political_data_txt_to_csv.py
@@ -1,4 +1,5 @@
 import csv
+import numpy as np
 import os
 from os import listdir
 from os.path import isfile, join
@@ -15,6 +16,19 @@ if __name__ == '__main__':
         '2013-2014',
     ]
 
+    columns = [[
+        'election type',
+        'entity',
+        'donor',
+        'city',
+        'state',
+        'zip code',
+        'recipient',
+        'recipient type',
+        'date',
+        'amount',
+    ]]
+
     os.mkdir(export_path)
 
     for year in years:
@@ -24,12 +38,26 @@ if __name__ == '__main__':
 
         os.mkdir(export_path_year)
 
-        filenames = [f.split('.')[0] for f in listdir(data_path_year) if isfile(join(data_path_year, f))]
+        filenames = [f.split('.')[0] for f in listdir(data_path_year) if isfile(join(data_path_year, f)) and 'dates' not in f]
 
         for fn in filenames:
             with open(f'{data_path_year}/{fn}.txt', 'r') as in_file:
                 stripped = (line.strip() for line in in_file)
-                lines = (line.split("|") for line in stripped if line)
+                lines = []
+
+                for line in stripped:
+                    if line:
+                        line_data = line.split('|')
+                        if len(line_data) == 20:
+                            line = np.array(line_data)[np.r_[3,5:14]]
+                        if len(line_data) == 21:
+                            line = np.array(line_data)[np.r_[3,6:15]]
+                        
+                        lines.append(line)
+
+                lines = tuple(lines)
+
                 with open(f'{export_path_year}/{fn}.csv', 'w') as out_file:
                     writer = csv.writer(out_file)
+                    writer.writerows(columns)
                     writer.writerows(lines)
