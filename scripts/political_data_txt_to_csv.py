25148c1ed73375798963cf044757c8f25129d6aa
Nelson Ding <nding17@outlook.com>
2020-01-08 17:52:45 -0500

    fixed bugs

--- scripts/political_data_txt_to_csv.py
+++ scripts/political_data_txt_to_csv.py
@@ -10,8 +10,6 @@ if __name__ == '__main__':
     data_path = '../data/Political_contributions'
     export_path = '../data/Political_contributions_CSV'
 
-    years = listdir(data_path)
-
     columns = [[
         'election type',
         'entity',
@@ -27,6 +25,8 @@ if __name__ == '__main__':
 
     os.mkdir(export_path)
 
+    years = listdir(data_path)
+
     for year in years:
 
         data_path_year = f'{data_path}/{year}'
@@ -37,13 +37,13 @@ if __name__ == '__main__':
         filenames = [f.split('.')[0] for f in listdir(data_path_year) if isfile(join(data_path_year, f)) and 'dates' not in f]
 
         for fn in filenames:
-            with open(f'{data_path_year}/{fn}.txt', 'r') as in_file:
+            with open(f'{data_path_year}/{fn}.txt', 'rb') as in_file:
                 stripped = (line.strip() for line in in_file)
                 lines = []
 
                 for line in stripped:
                     if line:
-                        line_data = line.split('|')
+                        line_data = str(line).split('|')
                         if len(line_data) == 20:
                             line = np.array(line_data)[np.r_[3,5:14]]
                         if len(line_data) == 21:
@@ -58,8 +58,8 @@ if __name__ == '__main__':
                     writer.writerows(columns)
                     writer.writerows(lines)
 
-            pd.read_csv(f'{export_path_year}/{fn}')\
+            pd.read_csv(f'{export_path_year}/{fn}.csv', index_col=0)\
               .drop_duplicates()\
               .query('amount>0')\
               .reset_index()\
-              .to_csv(f'{export_path_year}/{fn}')
+              .to_csv(f'{export_path_year}/{fn}.csv', index=False)
