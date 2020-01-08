21f6fd8a813509f0eed0df343cfa154b0b81e244
Nelson Ding <nding17@outlook.com>
2020-01-07 23:36:16 -0500

    more data years

--- scripts/political_data_txt_to_csv.py
+++ scripts/political_data_txt_to_csv.py
@@ -5,17 +5,31 @@ from os.path import isfile, join
 
 if __name__ == '__main__':
 
-    data_path = '../data/Political_contributions_2019'
-    export_path = '../data/political_data_2019_csv'
+    data_path = '../data/Political_contributions'
+    export_path = '../data/Political_contributions_CSV'
+
+    years = [
+        '2019',
+        '2017-2018',
+        '2015-2016',
+        '2013-2014',
+    ]
 
     os.mkdir(export_path)
 
-    filenames = [f.split('.')[0] for f in listdir(data_path) if isfile(join(data_path, f))]
+    for year in years:
 
-    for fn in filenames:
-        with open(f'{data_path}/{fn}.txt', 'r') as in_file:
-            stripped = (line.strip() for line in in_file)
-            lines = (line.split("|") for line in stripped if line)
-            with open(f'{export_path}/{fn}.csv', 'w') as out_file:
-                writer = csv.writer(out_file)
-                writer.writerows(lines)
+        data_path_year = f'{data_path}/{year}'
+        export_path_year = f'{export_path}/{year}'
+
+        os.mkdir(export_path_year)
+
+        filenames = [f.split('.')[0] for f in listdir(data_path_year) if isfile(join(data_path_year, f))]
+
+        for fn in filenames:
+            with open(f'{data_path_year}/{fn}.txt', 'r') as in_file:
+                stripped = (line.strip() for line in in_file)
+                lines = (line.split("|") for line in stripped if line)
+                with open(f'{export_path_year}/{fn}.csv', 'w') as out_file:
+                    writer = csv.writer(out_file)
+                    writer.writerows(lines)
