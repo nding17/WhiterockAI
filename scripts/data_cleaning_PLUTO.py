47dad5b08350590577bdf0d43e35f4f0c4fee66e
Nelson Ding <nding17@outlook.com>
2020-01-08 21:43:54 -0500

    data type conversion corrected

--- scripts/data_cleaning_PLUTO.py
+++ scripts/data_cleaning_PLUTO.py
@@ -14,14 +14,19 @@ if __name__ == '__main__':
                               (pluto['RETAIL SF']>0))].index
 
         df_cleaned =  df.drop(drop_indices)\
-                        .astype(dtype={'ZIP': str}, 
-                                errors='ignore')\
                         .reset_index()
+
+        df_cleaned.ZIP = df_cleaned.ZIP\
+                                   .fillna(0)\
+                                   .astype('int64')\
+                                   .astype(str)
 
         return df_cleaned
 
 
-    pluto = pd.read_csv('../data/project/NPL-001 All_Properties [bylocation;address] PLUTO.csv', index_col=0)
+    pluto = pd.read_csv('../data/project/NPL-001 All_Properties [bylocation;address] PLUTO.csv', 
+                        index_col=0,
+                        error_bad_lines=False)
 
     pluto_cleaned = clean_pluto(pluto)
     pluto_cleaned.to_csv('../data/project/All_Properties PLUTO_New_Data.csv', index=False)
