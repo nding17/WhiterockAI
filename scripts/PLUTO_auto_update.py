7c3ff8d1d462ab8cc8217a31815c0eb0dc6f55eb
Nelson Ding <nding17@outlook.com>
2020-01-17 17:10:57 -0500

    bug fi

--- scripts/PLUTO_auto_update.py
+++ scripts/PLUTO_auto_update.py
@@ -367,13 +367,13 @@ class clean_instructions:
     }
 
     log_dict = {
-        'download_df': 'downloading new data... (takes a while, please be patient)',
-        'pre_clean_df': 'pre-process the downloaded data (delete redundant columns, rename some columns)',
-        'subset_df_date': 'subset recent data from the downloaded data',
-        'load_old_PLUTO': 'loading old PLUTO data...',
-        'update_PLUTO': 'update PLUTO with the new data, data merging... (takes a while, please be patient)',
-        'process_PLUTO': 'final step: process the new pluto, almost ready',
-        'export_new_PLUTO': 'exporting PLUTO, job done!',
+        'download_df': '==>downloading new data... (takes a while, please be patient)',
+        'pre_clean_df': '==>pre-process the downloaded data (delete redundant columns, rename some columns)',
+        'subset_df_date': '==>subset recent data from the downloaded data',
+        'load_old_PLUTO': '==>loading old PLUTO data...',
+        'update_PLUTO': '==>update PLUTO with the new data, data merging... (takes a while, please be patient)',
+        'process_PLUTO': '==>final step: process the new pluto, almost ready',
+        'export_new_PLUTO': '==>exporting PLUTO, job done!',
     }
 
 
