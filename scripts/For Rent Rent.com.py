23d8a669b7fac4709ffc2097e1ee644e236d5b2d
Nelson Ding <nding17@outlook.com>
2020-02-06 23:37:33 -0500

    update version

--- scripts/For Rent Rent.com.py
+++ scripts/For Rent Rent.com.py
@@ -10,7 +10,7 @@ looking at.
 __author__ = 'Naili Ding'
 __email__ = 'nd2588@columbia.edu'
 __maintainer__ = 'Naili Ding'
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 __status__ = 'completed'
 
 import pandas as pd
