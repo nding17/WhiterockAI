89509b78346c10a072f996b3b6b939cf2fa838eb
Nelson Ding <nding17@outlook.com>
2020-02-25 16:19:07 -0500

    trulia deleted redundant code

--- scripts/data scraping/master_data_scraping.py
+++ scripts/data scraping/master_data_scraping.py
@@ -1802,60 +1802,6 @@ class trulia_dot_com(dot_com):
     # private functions section #
     #############################
 
-    @staticmethod
-    def _build_options():
-        options = webdriver.ChromeOptions()
-        options.accept_untrusted_certs = True
-        options.assume_untrusted_cert_issuer = True
-
-        # chrome configuration
-        # More: https://github.com/SeleniumHQ/docker-selenium/issues/89
-        # And: https://github.com/SeleniumHQ/docker-selenium/issues/87
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-impl-side-painting")
-        options.add_argument("--disable-setuid-sandbox")
-        options.add_argument("--disable-seccomp-filter-sandbox")
-        options.add_argument("--disable-breakpad")
-        options.add_argument("--disable-client-side-phishing-detection")
-        options.add_argument("--disable-cast")
-        options.add_argument("--disable-cast-streaming-hw-encoding")
-        options.add_argument("--disable-cloud-import")
-        options.add_argument("--disable-popup-blocking")
-        options.add_argument("--ignore-certificate-errors")
-        options.add_argument("--disable-session-crashed-bubble")
-        options.add_argument("--disable-ipv6")
-        options.add_argument("--allow-http-screen-capture")
-        options.add_argument("--start-maximized")
-        options.add_argument('--lang=es')
-
-        return options
-
-    def _get_browser(self, webpage):
-        """
-        A helper function to get the selenium browser in order 
-        to perform the scraping tasks 
-
-        Parameters
-        ----------
-        chromedriver : str
-            the path to the location of the chromedriver 
-
-        Returns
-        -------
-        browser : webdriver.Chrome
-            a chrome web driver 
-
-        wait : WebDriverWait
-            this is wait object that allows the program to hang around for a period
-            of time since we need some time to listen to the server 
-
-        """
-        options = self._build_options()
-        browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-        browser.get(webpage)
-        wait = WebDriverWait(browser, 20) # maximum wait time is 20 seconds 
-        return browser, wait
-
     def _get_buy_webpage(self, pg_num, htype):
 
         """
