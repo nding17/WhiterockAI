e071f2779c213f64fa5e11e24b74632721eb1979
Nelson Ding <nding17@outlook.com>
2020-02-26 10:33:42 -0500

    fixed language button issues

--- scripts/data scraping/master_data_scraping.py
+++ scripts/data scraping/master_data_scraping.py
@@ -460,6 +460,11 @@ class dot_com:
 
         """
         options = self._build_options()
+
+        # alternative option for Windows users 
+        # chrome_path = 'C:/Users/jorda/.wdm/drivers/chromedriver/79.0.3945.36/win32/chromedriver.exe'
+        # browser = webdriver.Chrome(executable_path = chrome_path, options=options)
+
         browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
         browser.get(webpage)
         wait = WebDriverWait(browser, 20) # maximum wait time is 20 seconds 
@@ -4551,10 +4556,20 @@ class apartments_dot_com(dot_com):
         self._state = self._state.lower()
         self._url = f'https://www.apartments.com/{self._city}-{self._state}/'
         self._browser, _ = self._get_browser(self._url)
+        
+    # choose the right language option
+    def _choose_lang(self, browser):
+        try:
+            lang_button = browser.find_element_by_xpath("//section[@class='languageConfirmation']") \
+                                 .find_element_by_xpath("//button[text()='English']")
+            lang_button.click()
+        except:
+            print('no language button')
 
     # scrape the apartment urls for the entire webpage 
     def _get_apt_urls(self, test=False):
         browser = self._browser
+        self._choose_lang(browser)
         max_pg = browser.find_element_by_xpath("//nav[@id='paging']") \
                         .find_elements_by_tag_name('li')[-2] \
                         .text
@@ -4686,6 +4701,7 @@ class apartments_dot_com(dot_com):
     def _get_apt_data(self, apt_url, img_path):
         browser = self._browser
         browser.get(apt_url)
+        self._choose_lang(browser)
         street, city, state, zipcode = self._get_address(browser)
         year_built, num_units, num_stories, fireplace, wd = self._get_features(browser)
         
@@ -5220,70 +5236,70 @@ if __name__ == '__main__':
     """
 
     major_city = 'CHI'
-
+    
     # user need to provide these paths 
     # please also make sure you have the sub-folders
     # under img_path, for example, remax, rent etc. 
     data_path = f'../../data/sample/{major_city}/info'
     img_path = f'../../data/sample/{major_city}/images'
-
+    
     # to run the scraping for the entire webpage 
     # turn this to False
     is_testing = True
-
-    ### compass New York For Rent 
-    codc = compass_dot_com(major_city)
-    codc.scraping_pipeline(data_path, f'{img_path}/compass', test=is_testing)
-
-    ### compass New York For Sale 
-    codcv2 = compass_fs_dot_com(major_city)
-    codcv2.scraping_pipeline(data_path, f'{img_path}/compass', test=is_testing)
-
-    ### remax.com Philadelphia For Sale
-    rmdc = remax_dot_com(major_city)
-    rmdc.scraping_pipeline(data_path, f'{img_path}/remax', test=is_testing)
-
-    # berkshire hathaway New York For Sale
-    bdc = berkshire_dot_com(major_city)
-    bdc.scraping_pipeline(data_path, f'{img_path}/berkshire', test=is_testing)
-
+    
     ### apartments.com New York For Rent
     adc = apartments_dot_com(major_city)
     adc.scraping_pipeline(data_path, f'{img_path}/apartments', test=is_testing)
-
+    
+    ### compass New York For Rent 
+    codc = compass_dot_com(major_city)
+    codc.scraping_pipeline(data_path, f'{img_path}/compass', test=is_testing)
+    
+    ### compass New York For Sale 
+    codcv2 = compass_fs_dot_com(major_city)
+    codcv2.scraping_pipeline(data_path, f'{img_path}/compass', test=is_testing)
+    
+    ### remax.com Philadelphia For Sale
+    rmdc = remax_dot_com(major_city)
+    rmdc.scraping_pipeline(data_path, f'{img_path}/remax', test=is_testing)
+    
+    # berkshire hathaway New York For Sale
+    bdc = berkshire_dot_com(major_city)
+    bdc.scraping_pipeline(data_path, f'{img_path}/berkshire', test=is_testing)
+    
     ### elliman.com For Sale 
     if major_city == 'NYC':
         edc = elliman_dot_com(major_city)
         edc.scraping_pipeline(data_path, f'{img_path}/elliman', test=is_testing)
-
+    
     ### loopnet.com New York For Sale 
     ldc = loopnet_dot_com(major_city)
     ldc.scraping_pipeline(data_path, f'{img_path}/loopnet', test=is_testing)
-
+    
     ### rent.com Philadelphia For Rent
     rdc = rent_dot_com(major_city)
     rdc.scraping_pipeline(data_path, f'{img_path}/rent', test=is_testing)
-
+    
     ### coldwell Philadelphia For Sale
     cdc = coldwell_dot_com(major_city, 1, 'max')
     cdc.scraping_pipeline(data_path, f'{img_path}/coldwell', test=is_testing)
-
+    
     ### hotpads.com For Rent
     hdc = hotpads_dot_com(major_city)
     hdc.scraping_pipeline(data_path, f'{img_path}/hotpads', test=is_testing)
-
+    
     ### trulia.com For Rent and For Sale
     tdc = trulia_dot_com(major_city, 'buy')
     tdc.scraping_pipeline(data_path, f'{img_path}/trulia', test=is_testing)
-
+    
     ### trulia.com For Rent and For Rent
     tdc = trulia_dot_com(major_city, 'rent')
     tdc.scraping_pipeline(data_path, f'{img_path}/trulia', test=is_testing)
-
+    
     ### trulia.com For Rent and Sold
     tdc = trulia_dot_com(major_city, 'sold')
     tdc.scraping_pipeline(data_path, f'{img_path}/trulia', test=is_testing)
-
+    
     ### merge all the datafiles into a master data file 
     dm = data_merger(data_path)
     dm.merge_super_dfs(major_city)
