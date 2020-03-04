b743c403c0e867c11987e71c36867fe077e8ee78
Nelson Ding <nding17@outlook.com>
2020-03-04 10:49:34 -0500

    removed redundant

---
+++ scripts/data processing/CHI_PLUTO_auto_update.py
@@ -0,0 +1,21 @@
+### package requirements
+import pandas as pd
+import numpy as np
+import warnings
+import urllib
+import os
+import time
+import requests
+
+from datetime import datetime
+from datetime import date
+from urllib.request import urlopen
+from lycleaner import Address_cleaner
+
+class cleaning_instructions:
+
+	CHI_PLUTO_CLEANING = {
+		
+
+	}
+
