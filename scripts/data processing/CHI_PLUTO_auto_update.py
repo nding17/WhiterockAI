d207b1586cee9de9c50a162374ba532ec8ce7091
Nelson Ding <nding17@outlook.com>
2020-03-04 15:23:33 -0500

    some bug fixes along the way and instructions update

--- scripts/data processing/CHI_PLUTO_auto_update.py
+++ scripts/data processing/CHI_PLUTO_auto_update.py
@@ -11,342 +11,416 @@ from datetime import datetime
 from datetime import date
 from urllib.request import urlopen
 from lycleaner import Address_cleaner
+from sodapy import Socrata
 
 class cleaning_instructions:
 
-	CHI_PLUTO_CLEANING = {
-		'PIN': {
-			'delete': 0,
-			'new name': 'PIN',
-		},
-		'Property Class': {
-			'delete': 0,
-			'new name': 'BLDG CODE',
-		},
-		'Tax Year': {
-			'delete': 0,
-			'new name': 'TAX YEAR',
-		},
-		'Neighborhood Code': {
-			'delete': 0,
-			'new name': 'NEIGHBORHOOD',
-		},
-		'Land Square Feet': {
-			'delete': 0,
-			'new name': 'LAND SF',
-		},
-		'Town Code': {
-			'delete': 0,
-			'new name': 'TOWN',
-		},
-		'Type of Residence': {
-			'delete': 0,
-			'new name': 'BLDG CAT',
-		},
-		'Apartments': {
-			'delete': 0,
-			'new name': '# UNITS',
-		},
-		'Wall Material': {
-			'delete': 0,
-			'new name': 'EXT MATERIAL',
-		},
-		'Roof Material': {
-			'delete': 0,
-			'new name': 'ROOF MATERIAL',
-		},
-		'Rooms': {
-			'delete': 0,
-			'new name': '# ROOMS',
-		},
-		'Bedrooms': {
-			'delete': 0,
-			'new name': '# BED',
-		},
-		'Basement': {
-			'delete': 0,
-			'new name': 'BASEMENT',
-		},
-		'Basement Finish': {
-			'delete': 0,
-			'new name': 'BASEMENT COND',
-		},
-		'Central Heating': {
-			'delete': 0,
-			'new name': 'CENTRAL HEATING',
-		},
-		'Other Heating': {
-			'delete': 0,
-			'new name': 'OTHER HEATING',
-		},
-		'Central Air': {
-			'delete': 0,
-			'new name': 'CENTRAL AIR',
-		},
-		'Fireplaces': {
-			'delete': 0,
-			'new name': '# FIREPLACE',
-		},
-		'Attic Type': {
-			'delete': 0,
-			'new name': 'ATTIC',
-		},
-		'Attic Finish': {
-			'delete': 0,
-			'new name': 'ATTIC COND',
-		},
-		'Half Baths': {
-			'delete': 0,
-			'new name': '# POWDER',
-		},
-		'Design Plan': {
-			'delete': 1,
-		},
-		'Cathedral Ceiling': {	
-			'delete': 0,
-			'new name': 'CATHEDRAL CEILING',
-		},
-		'Construction Quality': {
-			'delete': 0,
-			'new name': 'CONST QUALITY',
-		},
-		'Renovation': {
-			'delete': 1,
-		},
-		'Site Desireability': {
-			'delete': 1,
-		},
-		'Garage 1 Size': {
-			'delete': 0,
-			'new name': 'GARAGE 1',
-		},
-		'Garage 1 Material': {
-			'delete': 0,
-			'new name': 'GARAGE 1 MATERIAL',
-		},
-		'Garage 1 Attachment': {
-			'delete': 0,
-			'new name': 'GARAGE ATTACHED',
-		},
-		'Garage 1 Area': {
-			'delete': 0,
-			'new name': 'GARAGE 1 AREA'
-		},
-		'Garage 2 Size': {
-			'delete': 1,
-		},
-		'Garage 2 Material': {
-			'delete': 1,
-		},
-		'Garage 2 Attachment': {
-			'delete': 1,
-		},
-		'Garage 2 Area': {
-			'delete': 1,
-		},
-		'Porch': {
-			'delete': 0,
-			'new name': 'ENCLOSED PORCH',
-		},
-		'Other Improvements': {
-			'delete': 1,
-		},
-		'Building Square Feet': {
-			'delete': 0,
-			'new name': 'GSF',
-		},
-		'Repair Condition': {
-			'delete': 0,
-			'new name': 'INT COND',
-		},
-		'Multi Code': {
-			'delete': 0,
-			'new name': '# BLDG',
-		},
-		'Number of Commercial Units': {
-			'delete': 0,
-			'new name': '# COMM',
-		},
-		'Prior Tax Year Market Value Estimate (Land)': {
-			'delete': 0,
-			'new name': 'LAND ASSD $ PY',
-		},
-		'Prior Tax Year Market Value Estimate (Building)': {
-			'delete': 0,
-			'new name': 'BLDG ASSD $ PY',
-		},
-		'Date of Most Recent Sale': {
-			'delete': 0,
-			'new name': 'SALE DATE',
-		},
-		'Deed No.': {
-			'delete': 0,
-			'new name': 'Deed No.',
-		},
-		'Sale Price': {
-			'delete': 0,
-			'new name': 'SALE PRICE',
-		},
-		'Longitude': {
-			'delete': 0,
-			'new name': 'LONGITUDE',
-		},
-		'Latitude': {
-			'delete': 0,
-			'new name': 'LATITUDE',
-		},
-		'Census Tract': {
-			'delete': 1,
-		},
-		'Total Building Square Feet': {
-			'delete': 0,
-			'new name': 'Total Building Square Feet',
-		},
-		'Multi Property Indicator': {
-			'delete': 1,
-		},
-		'Property Address': {
-			'delete': 0,
-			'new name': 'ADDRESS',
-		},
-		'Modeling Group': {
-			'delete': 0,
-			'new name': 'Modeling Group',
-		},
-		'Full Baths': {
-			'delete': 0,
-			'new name': '# BATH',
-		},
-		'Age': {
-			'delete': 0,
-			'new name': 'YEAR BUILT',
-		},
-		'Use': {
-			'delete': 0,
-			'new name': 'PROPERTY TYPE',
-		},
-		'Number of Units': {
-			'delete': 0,
-			'new name': '# UNITS',
-		},
-		'Percent Ownership': {
-			'delete': 1,
-		},
-		'Condo Class Factor': {
-			'delete': 1,
-		},
-		'Multi-Family Indicator': {
-			'delete': 1,
-		},
-		'Large Lot': {
-			'delete': 1,
-		},
-		'Condition, Desirability and Utility': {
-			'delete': 1,
-		},
-		'Deed Type': {
-			'delete': 1,
-		},
-		"O'Hare Noise": {
-			'delete': 0,
-			'new name': 'AIRPORT NOISE',
-		},
-		'Floodplain': {
-			'delete': 0,
-			'new name': 'FEMA',
-		},
-		'Near Major Road': {
-			'delete': 0,
-			'new name': 'Near Major Road',
-		},
-		'Total Units': {
-			'delete': 1,
-		},
-		'Condo Strata': {
-			'delete': 1,
-		},
-		'Age Squared': {
-			'delete': 1,
-		},
-		'Age Decade': {
-			'delete': 1,
-		},
-		'Age Decade Squared': {
-			'delete': 1,
-		},
-		'Lot Size Squared': {
-			'delete': 1,
-		},
-		'Improvement Size Squared': {
-			'delete': 1,
-		},
-		'Location Factor': {
-			'delete': 1,
-		},
-		'Garage indicator': {
-			'delete': 1,
-		},
-		'Residential share of building': {
-			'delete': 1,
-		},
-		'Pure Market Sale': {
-			'delete': 1,
-		},
-		'Pure Market Filter': {
-			'delete': 1,
-		},
-		'Neigborhood Code (mapping)': {
-			'delete': 1,
-		},
-		'Square root of lot size': {
-			'delete': 1
-		},
-		'Square root of age': {
-			'delete': 1,
-		},
-		'Square root of age': {
-			'delete': 1,
-		},
-		'Square root of improvement size': {
-			'delete': 1
-		},
-		'Town and Neighborhood': {
-			'delete': 1,
-		}
-	}
+    CHI_PLUTO_CLEANING = {
+        'pin': {
+            'delete': 0,
+            'new name': 'PIN',
+        },
+        'class': {
+            'delete': 0,
+            'new name': 'BLDG CODE',
+        },
+        'tax_year': {
+            'delete': 0,
+            'new name': 'TAX YEAR',
+        },
+        'nbhd': {
+            'delete': 0,
+            'new name': 'NEIGHBORHOOD',
+        },
+        'hd_sf': {
+            'delete': 0,
+            'new name': 'LAND SF',
+        },
+        'town_code': {
+            'delete': 0,
+            'new name': 'TOWN',
+        },
+        'type_resd': {
+            'delete': 0,
+            'new name': 'BLDG CAT',
+        },
+        'apts': {
+            'delete': 0,
+            'new name': '# UNITS',
+        },
+        'ext_wall': {
+            'delete': 0,
+            'new name': 'EXT MATERIAL',
+        },
+        'roof_cnst': {
+            'delete': 0,
+            'new name': 'ROOF MATERIAL',
+        },
+        'rooms': {
+            'delete': 0,
+            'new name': '# ROOMS',
+        },
+        'beds': {
+            'delete': 0,
+            'new name': '# BED',
+        },
+        'bsmt': {
+            'delete': 0,
+            'new name': 'BASEMENT',
+        },
+        'bsmt_fin': {
+            'delete': 0,
+            'new name': 'BASEMENT COND',
+        },
+        'heat': {
+            'delete': 0,
+            'new name': 'CENTRAL HEATING',
+        },
+        'oheat': {
+            'delete': 0,
+            'new name': 'OTHER HEATING',
+        },
+        'air': {
+            'delete': 0,
+            'new name': 'CENTRAL AIR',
+        },
+        'frpl': {
+            'delete': 0,
+            'new name': '# FIREPLACE',
+        },
+        'attic_type': {
+            'delete': 0,
+            'new name': 'ATTIC',
+        },
+        'attic_fnsh': {
+            'delete': 0,
+            'new name': 'ATTIC COND',
+        },
+        'hbath': {
+            'delete': 0,
+            'new name': '# POWDER',
+        },
+        'tp_plan': {
+            'delete': 1,
+        },
+        'tp_dsgn': {  
+            'delete': 0,
+            'new name': 'CATHEDRAL CEILING',
+        },
+        'cnst_qlty': {
+            'delete': 0,
+            'new name': 'CONST QUALITY',
+        },
+        'renovation': {
+            'delete': 1,
+        },
+        'site': {
+            'delete': 1,
+        },
+        'gar1_size': {
+            'delete': 0,
+            'new name': 'GARAGE 1',
+        },
+        'gar1_cnst': {
+            'delete': 0,
+            'new name': 'GARAGE 1 MATERIAL',
+        },
+        'gar1_att': {
+            'delete': 0,
+            'new name': 'GARAGE ATTACHED',
+        },
+        'gar1_area': {
+            'delete': 0,
+            'new name': 'GARAGE 1 AREA'
+        },
+        'gar2_size': {
+            'delete': 1,
+        },
+        'gar2_cnst': {
+            'delete': 1,
+        },
+        'gar2_att': {
+            'delete': 1,
+        },
+        'gar2_area': {
+            'delete': 1,
+        },
+        'porch': {
+            'delete': 0,
+            'new name': 'ENCLOSED PORCH',
+        },
+        'ot_impr': {
+            'delete': 1,
+        },
+        'bldg_sf': {
+            'delete': 0,
+            'new name': 'GSF',
+        },
+        'repair_cnd': {
+            'delete': 0,
+            'new name': 'INT COND',
+        },
+        'multi_code': {
+            'delete': 0,
+            'new name': '# BLDG',
+        },
+        'ncu': {
+            'delete': 0,
+            'new name': '# COMM',
+        },
+        'pri_est_land': {
+            'delete': 0,
+            'new name': 'LAND ASSD $ PY',
+        },
+        'pri_est_bldg': {
+            'delete': 0,
+            'new name': 'BLDG ASSD $ PY',
+        },
+        'most_recent_sale_date': {
+            'delete': 0,
+            'new name': 'SALE DATE',
+        },
+        'doc_no': {
+            'delete': 0,
+            'new name': 'Deed No.',
+        },
+        'most_recent_sale_price': {
+            'delete': 0,
+            'new name': 'SALE PRICE',
+        },
+        'centroid_x': {
+            'delete': 0,
+            'new name': 'LONGITUDE',
+        },
+        'centroid_y': {
+            'delete': 0,
+            'new name': 'LATITUDE',
+        },
+        'tractce': {
+            'delete': 1,
+        },
+        'total_bldg_sf': {
+            'delete': 0,
+            'new name': 'Total Building Square Feet',
+        },
+        'multi_ind': {
+            'delete': 1,
+        },
+        'addr': {
+            'delete': 0,
+            'new name': 'ADDRESS',
+        },
+        'modeling_group': {
+            'delete': 0,
+            'new name': 'Modeling Group',
+        },
+        'fbath': {
+            'delete': 0,
+            'new name': '# BATH',
+        },
+        'age': {
+            'delete': 0,
+            'new name': 'YEAR BUILT',
+        },
+        'use_1': {
+            'delete': 0,
+            'new name': 'PROPERTY TYPE',
+        },
+        'n_units': {
+            'delete': 0,
+            'new name': '# UNITS',
+        },
+        'per_ass': {
+            'delete': 1,
+        },
+        'condo_class_factor': {
+            'delete': 1,
+        },
+        'multi_family_ind': {
+            'delete': 1,
+        },
+        'large_lot': {
+            'delete': 1,
+        },
+        'deed_type': {
+            'delete': 1,
+        },
+        'o_hare_noise': {
+            'delete': 0,
+            'new name': 'AIRPORT NOISE',
+        },
+        'floodplain': {
+            'delete': 0,
+            'new name': 'FEMA',
+        },
+        'near_major_road': {
+            'delete': 0,
+            'new name': 'Near Major Road',
+        },
+        'total_units': {
+            'delete': 1,
+        },
+        'condo_strata': {
+            'delete': 1,
+        },
+        'age_squared': {
+            'delete': 1,
+        },
+        'age_decade': {
+            'delete': 1,
+        },
+        'age_decade_squared': {
+            'delete': 1,
+        },
+        'lot_size_squared': {
+            'delete': 1,
+        },
+        'improvement_size_squared': {
+            'delete': 1,
+        },
+        'location_factor': {
+            'delete': 1,
+        },
+        'garage_indicator': {
+            'delete': 1,
+        },
+        'residential_share_of_building': {
+            'delete': 1,
+        },
+        'pure_market_sale': {
+            'delete': 1,
+        },
+        'pure_market_filter': {
+            'delete': 1,
+        },
+        'neigborhood_code_mapping_': {
+            'delete': 1,
+        },
+        'square_root_of_lot_size': {
+            'delete': 1
+        },
+        'square_root_of_age': {
+            'delete': 1,
+        },
+        'square_root_of_improvement_size': {
+            'delete': 1
+        },
+        'town_and_neighborhood': {
+            'delete': 1,
+        }
+    }
 
-	CHI_SALES_CLEANING = CHI_PLUTO_CLEANING.update(
-		{
-			'Sale Date': {
-				'delete': 0,
-				'new name': 'SALE DATE',
-			},
-			'Sale Year': {
-				'delete': 1,
-			},
-			'Sale Quarter': {
-				'delete': 1,
-			},
-			'Sale Half-Year': {
-				'delete': 1,
-			},
-			'Sale Quarter of Year': {
-				'delete': 1,
-			},
-			'Sale Month of Year': {
-				'delete': 1,
-			},
-			'Sale Half of Year': {
-				'delete': 1,
-			},
-			'Town and Neighborhood, SF Group Only': {
-				'delete': 1,
-			}
-		}
-	)
+    CHI_SALES_CLEANING_ADD = {
+        'sale_date': {
+            'delete': 0,
+            'new name': 'SALE DATE',
+        },
+        'sale_year': {
+            'delete': 1,
+        },
+        'sale_quarter': {
+            'delete': 1,
+        },
+        'sale_half_year': {
+            'delete': 1,
+        },
+        'sale_quarter_of_year': {
+            'delete': 1,
+        },
+        'sale_month_of_year': {
+            'delete': 1,
+        },
+        'sale_half_of_year': {
+            'delete': 1,
+        },
+        'town_and_neighborhood_sf_group_only': {
+            'delete': 1,
+        },
+        'most_recent_sale': {
+            'delete': 0,
+            'new name': 'most_recent_sale'
+        },
+        'road_proximity': {
+            'delete': 0,
+            'new name': 'road_proximity'
+        },
+        'est_bldg': {
+            'delete': 0,
+            'new name': 'est_bldg',
+        },
+        'est_land': {
+            'delete': 0,
+            'new name': 'est_land',
+        },
+        'sale_price': {
+            'delete': 0,
+            'new name': 'SALE PRICE',
+        }
+    }
 
-	instructions = {
-		'CHI_PLUTO_CLEANING': CHI_PLUTO_CLEANING,
-		'CHI_SALES_CLEANING': CHI_SALES_CLEANING,
-	}
+    # combine two different dictionaries 
+    CHI_SALES_CLEANING = {**CHI_PLUTO_CLEANING, **CHI_SALES_CLEANING_ADD}
+    
+    instructions = {
+        'CHI_PLUTO_CLEANING': CHI_PLUTO_CLEANING,
+        'CHI_SALES_CLEANING': CHI_SALES_CLEANING,
+    }
 
+class cleaning_pipeline:
+
+    def __init__(self):
+        self._chi_pluto_api_id = 'bcnq-qi2z'
+        self._chi_sales_api_id = '5pge-nu6u'
+
+    ### extract data from API 
+    def _extract_data(self, api_id):
+        # MAX_LIMIT = 1000000000
+        MAX_LIMIT = 100000
+        client = Socrata("datacatalog.cookcountyil.gov", None)
+        results = client.get(api_id, limit=MAX_LIMIT)
+        data = pd.DataFrame.from_records(results)
+        return data
+
+    ### standardizing the column names of the dataframes 
+    def _clean_data(self, data, ins):
+        orig_cols = [*ins.keys()] # original column names to be updated
+        valid_cols = list(set(orig_cols).intersection(set(data.columns)))
+        newdata = data.copy()[valid_cols] # sales data cleaned 
+            
+        for column in valid_cols:
+            if ins[column]['delete'] == 1:
+                newdata = newdata.drop([column], axis=1)
+            if ins[column]['delete'] == 0:
+                newdata = newdata.rename(columns={column: ins[column]['new name']})
+
+        newdata_cl = newdata.drop_duplicates(subset=['PIN', 'ADDRESS', 'BLDG CODE', 'LAND SF'])
+        print(newdata.shape, newdata_cl.shape)
+        return newdata
+
+    def pipeline_data(self, api_id, ins):
+        data = self._extract_data(api_id)
+        newdata = self._clean_data(data, ins)
+        return newdata
+
+    ### load the base PLUTO data file for update
+    def _load_old_pluto(self, pluto_path):
+        pluto = pd.read_csv(f'{pluto_path}/CHIPL-001 All_Properties PLUTO.csv', low_memory=False)
+        pluto = pluto.dropna(subset=['PIN', 'ADDRESS'])
+        pluto = pluto.drop_duplicates()
+        pluto['PIN'] = pluto['PIN'].astype(str, errors='ignore') \
+                                   .apply(lambda x: x.split('.')[0])
+
+        pluto_cl = pluto.sort_values(by='SALE DATE') \
+                        .drop_duplicates(subset=['PIN', 'ADDRESS', 'BLDG CODE', 'LAND SF'], 
+                                         keep='last')
+
+        return pluto_cl
+
+if __name__ == '__main__':
+    cp = cleaning_pipeline()
+    ci = cleaning_instructions()
+    ins = ci.instructions['CHI_PLUTO_CLEANING']
+    api_id = cp._chi_pluto_api_id
+    newdata = cp.pipeline_data(api_id, ins)
+
+    # cp._load_old_pluto('../../data/CHI Data')
